59e7c4109eddbea0093577099320bf9cf4e1e23c
Vinncent Miller <vinncentmiller@gmail.com>
2024-10-25 11:08:33 -0400

    Mutation to update quantity

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ import tamData from './tam.js'
 
 dotenv.config()
 
+async function getResponse(query) {
+    const response = await axios.post(
+        `https://${process.env.SHOP}.myshopify.com/admin/api/2024-10/graphql.json`,
+        {query},
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Access-Token': process.env.ADMIN_ACCESS_TOKEN
+            }
+        }
+    )
+    return response;
+}
+
 // return shopify variant id using tam sku
 async function fetchProductBySku(sku) {
     const query = ` query {
@@ -19,23 +33,17 @@ async function fetchProductBySku(sku) {
                     id
                     title
                     sku
+                    inventoryItem {
+                        id
+                    }
                 }
             }
         }
     }`
     try {
-        const response = await axios.post(
-            `https://${process.env.SHOP}.myshopify.com/admin/api/2024-10/graphql.json`,
-            {query},
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Shopify-Access-Token': process.env.ADMIN_ACCESS_TOKEN
-                }
-            }
-        )
+        const response = await getResponse(query)
         const product = response.data.data.productVariants.edges[0].node
-        return (product.id)
+        return (product)
     } catch (error) {
         console.error('Error fetching product', error)
     }
@@ -44,18 +52,40 @@ async function fetchProductBySku(sku) {
 async function updateItem(sku){
     // use sku to get product id+
     const item = await fetchProductBySku(sku)
+    const itemID = item.id
+    const inventoryID = item.inventoryItem.id
+    // change location to correct id
+    const location = "gid://shopify/Location/96155828471"
     const tamInfo = tamData(sku)
 
-    console.log(item, tamInfo)
-    // const updatedVariant = {
-    //     stock: tamInfo.qty
-    // }
-    // try {
-    //     const response = await shopify.productVariant.list(sku)
-    //     console.log('Product Variants:', response)
-    // } catch (error) {
-    //     console.error(error)
-    // }
+    // tamInfo.qty for stock
+    const query = `mutation {
+    inventorySetQuantities(input: {
+      ignoreCompareQuantity: true
+      name: "available"
+      reason: "correction"
+      quantities: [
+        {
+          inventoryItemId: "${inventoryID}"
+          locationId: "${location}"
+          quantity: ${tamInfo.qty * 0.1}
+        }
+      ]
+    }) {
+      userErrors {
+        field
+        message
+      }
+    }
+  }`
+  try {
+    const response = await getResponse(query)
+    // const product = response.data.data.productVariants.edges[0].node
+    console.log (response.data)
+    } catch (error) {
+        console.error('Error updating product', error)
+    }
+
 }
 
 updateItem(22189)
@@ -86,4 +116,6 @@ updateItem(22189)
 
 
 // ideas:
+// when creating new object make sure tracked is set to true
 // use AI to generate tags, category, type, etc
+// need to figure out batch updates to reduce api calls
